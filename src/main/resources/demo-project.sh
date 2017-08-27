7770a11e90d1303d7964717d92776207ebc54177
jisong zhu <jisongzhu@jisongdeMacBook-Pro.local>
2017-08-27 23:59:07 +0800

    first commit

--- src/main/resources/demo-project.sh
+++ src/main/resources/demo-project.sh
@@ -2,7 +2,7 @@
 JARPath=./coins-0.0.1-SNAPSHOT.jar
 APP_NAME=coins_project
 APPLICATION_FILE=/opt/scpip_monitor/application.properties
-PID=$(ps aux | grep ${_JAR_KEYWORDS} | grep -v grep | awk '{print $2}' )
+PID=$(ps aux | grep coins-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' )
 
 function check_if_process_is_running {
  if [ "$PID" = "" ]; then
@@ -17,24 +17,24 @@ case "$1" in
   status)
     if check_if_process_is_running
     then
-      echo -e "\033[32m $APP_NAME is running \033[0m"
+      echo -e " $APP_NAME is running "
     else
-      echo -e "\033[32m $APP_NAME not running \033[0m"
+      echo -e " $APP_NAME not running "
     fi
     ;;
   stop)
     if ! check_if_process_is_running
     then
-      echo  -e "\033[32m $APP_NAME  already stopped \033[0m"
+      echo  -e " $APP_NAME  already stopped "
       exit 0
     fi
     kill -9 $PID
-    echo -e "\033[32m Waiting for process to stop \033[0m"
+    echo -e " Waiting for process to stop "
     NOT_KILLED=1
     for i in {1..20}; do
       if check_if_process_is_running
       then
-        echo -ne "\033[32m . \033[0m"
+        echo -ne " . "
         sleep 1
       else
         NOT_KILLED=0
@@ -43,28 +43,28 @@ case "$1" in
     echo
     if [ $NOT_KILLED = 1 ]
     then
-      echo -e "\033[32m Cannot kill process \033[0m"
+      echo -e " Cannot kill process "
       exit 1
     fi
-    echo  -e "\033[32m $APP_NAME already stopped \033[0m"
+    echo  -e " $APP_NAME already stopped "
     ;;
   start)
     if [ "$PID" != "" ] && check_if_process_is_running
     then
-      echo -e "\033[32m $APP_NAME already running \033[0m"
+      echo -e " $APP_NAME already running "
       exit 1
     fi
    nohup java -jar $JARPath >/dev/null 2>&1 &
-   echo -ne "\033[32m Starting \033[0m"
+   echo -ne " Starting "
     for i in {1..20}; do
-        echo -ne "\033[32m.\033[0m"
+        echo -ne "."
         sleep 1
     done
     if check_if_process_is_running
      then
-       echo  -e "\033[32m $APP_NAME fail \033[0m"
+       echo  -e " $APP_NAME fail "
     else
-       echo  -e "\033[32m $APP_NAME started \033[0m"
+       echo  -e " $APP_NAME started "
     fi
     ;;
   restart)
